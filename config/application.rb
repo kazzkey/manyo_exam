fb22b1f1a66c1599abb930d81d645f6783c59863
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-05-23 17:49:08 +0900

    現状の機能についてE2Eテストを実施し、確認

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,13 @@ module TaskMgtApp
       g.assets false
       g.helper false
       g.jbuilder false
+      g.test_framework :rspec,
+        model_specs: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
     end
   end
 end
