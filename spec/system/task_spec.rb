f17afe17f20db3026964b03c8398c6ce44d0e50e
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-05-24 17:09:21 +0900

    タスク一覧が降順に並ぶかについてのテストを完了

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+  end
   describe 'タスク一覧画面' do
-    context 'タスクを作成した場合' do
+    context '複数のタスクを作成した場合' do
       it '作成済みのタスクが表示される' do
-        task = FactoryBot.create(:task, name: 'task')
         visit root_path
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'default_name1'
+        expect(page).to have_content 'default_name2'
+      end
+    end
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日時の降順に並んでいる' do
+        visit root_path
+        task_list = all('tbody tr')
+        expect(task_list[0]).to have_content 'default_name2'
+        expect(task_list[1]).to have_content 'default_name1'
       end
     end
   end
@@ -16,7 +28,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in 'Name', with: 'task_name'
         fill_in 'Content', with: 'task_content'
-        click_on 'Create Task'
+        click_on '登録する'
         expect(page).to have_content 'task_name'
         expect(page).to have_content 'task_content'
       end
@@ -25,11 +37,11 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク詳細画面' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示されたページに遷移する' do
-         task = FactoryBot.create(:task, name: 'task_name', content: 'task_content')
+         new_task = FactoryBot.create(:task, name: 'new_name', content: 'new_content')
          visit root_path
-         click_on "Show"
-         expect(page).to have_content 'task_name'
-         expect(page).to have_content 'task_content'
+         first('tbody tr').click_on '詳細'
+         expect(page).to have_content 'new_name'
+         expect(page).to have_content 'new_content'
        end
      end
   end
